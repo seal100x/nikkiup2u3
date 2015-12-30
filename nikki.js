63fc1501e795b932aeff337b191da87bd01cfbce
cty <seal100x@163.com>
2015-12-30 23:17:49 +0800

    init biguse

--- nikki.js
+++ nikki.js
@@ -157,30 +157,6 @@ function refreshTable() {
 	drawTable(filtering(criteria, uiFilter), "clothes", false);
 }
 
-function clone(obj) {
-	var o;
-	if (typeof obj == "object") {
-		if (obj === null) {
-			o = null;
-		} else {
-			if (obj instanceof Array) {
-				o = [];
-				for (var i = 0, len = obj.length; i < len; i++) {
-					o.push(clone(obj[i]));
-				}
-			} else {
-				o = {};
-				for (var j in obj) {
-					o[j] = clone(obj[j]);
-				}
-			}
-		}
-	} else {
-		o = obj;
-	}
-	return o;
-}
-
 function chooseAccessories(accfilters) {
 	shoppingCart.clear();
 	shoppingCart.putAll(filterTopAccessories(clone(accfilters)));
@@ -332,7 +308,7 @@ function filterTopAccessories(filters) {
 	toSort.sort(byScore);
 	var total = 0;
 	var maxTotal = 0;
-	var maxIdx = 0;
+	var maxIdx = -1;
 	for (var i = 0; i < toSort.length; i++) {
 		total += toSort[i].tmpScore;
 		realScore = accScore(total, i+1);
